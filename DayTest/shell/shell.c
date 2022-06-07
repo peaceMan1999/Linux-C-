6b75a24ae6158054b53770235d8d28b361b35af9
peaceMan1999 <984014088@qq.com>
2022-06-07 23:18:04 +0800

    添加重定向功能

--- DayTest/shell/shell.c
+++ DayTest/shell/shell.c
@@ -3,6 +3,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 #define NUM 1000
 #define LEN 32
@@ -13,9 +17,28 @@ int main()
     char* cmd[LEN];
     while(1){
         printf("[yz@VM-20-9-centos shell]# ");
-        fgets(name, NUM, stdin);
-       
+        fgets(name, NUM, stdin); 
         name[strlen(name)-1] = '\0';
+       
+        char *start = name;
+        while (*start != '\0'){
+            if (*start == '>'){
+                *start = '\0';
+                start++;
+                break;
+            }
+
+            start++;
+        }
+        if (*start != '\0'){
+            while (isspace(*start)){
+                start++;
+            }
+        }
+        else{
+            start = NULL;
+        }
+        
         cmd[0] = strtok(name, " ");
         int i = 1;
         while (cmd[i]= strtok(NULL, " ")){
@@ -23,7 +46,16 @@ int main()
         }
         pid_t id = fork();
         if (id == 0){
-            execvp("ls", cmd);
+            if (start != NULL){
+                int fd = open(start, O_WRONLY|O_CREAT, 0644);
+                if (fd < 0){
+                    perror("err");
+                    exit(1);
+                }
+                dup2(fd, 1);
+            }
+            execvp(cmd[0], cmd);
+            exit(0);
         }
         
         int status = 0;
